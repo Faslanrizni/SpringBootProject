ba7b9b891f85b9fab1f587f1698d2dad994c34b7
Faslanrizni <116054643+Faslanrizni@users.noreply.github.com>
2024-04-28 09:53:23 +0530

    applying security update 2.1

--- src/main/java/com/devstack/healthCare/product/security/ApplicationUserRole.java
+++ src/main/java/com/devstack/healthCare/product/security/ApplicationUserRole.java
@@ -1,10 +1,31 @@
 package com.devstack.healthCare.product.security;
 
+import com.google.common.collect.Sets;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public enum ApplicationUserRole {
-    DOCTOR,
-    ADMIN
+    DOCTOR(Sets.newHashSet()),
+    ADMIN(Sets.newHashSet());
+    private final Set<ApplicationUserPermission> permission;
 
-    private final Set<ApplicationUserPermission>
+    ApplicationUserRole(Set<ApplicationUserPermission> permission) {
+        this.permission = permission;
+    }
+
+    public Set<ApplicationUserPermission> getPermissions(){
+        return permission;
+
+
+    }
+    public Set<SimpleGrantedAuthority> getSimpleGrantedAuthorities(){
+        Set<SimpleGrantedAuthority> permissions= getPermissions()
+                .stream().map(permission->new
+                        SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE_"+this.name()));
+        return permissions;
+    }
 }
